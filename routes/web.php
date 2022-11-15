7e17a35696f3e28081df6ac2af98ede06df51a35
sakshiburghateNimap <sakshiburghate@nimapinfotech.com>
2022-11-15 10:54:32 +0530

    frist commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/blogs/{id}', [BlogController::class, 'index']);
+Route::post('/blogs/update/{id}', 'BlogController@update');
+Route::delete('/blogs/delete/{id}', 'BlogController@delete');
+
+
+
